2f07a270d924d2c2b50039d4cea4a34a4c06d224
Paul Stearns <coyote@Pauls-MBP.routerlogin.net>
2022-06-21 12:04:35 -0400

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import Tetris from './components/Tetris';
+import './App.css';
+
+const App = () => {
+
+    return (
+        <div className="App">
+            <Tetris />
+        </div>
+    );
+
+}
+
+export default App;
---
+++ src/components/Stage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Cell from './Cell';
+
+
+const Stage = ({ stage }) => {
+
+    return (
+        <div>
+            <Cell />
+        </div>
+    );
+
+};
+
+
+export default Stage;
